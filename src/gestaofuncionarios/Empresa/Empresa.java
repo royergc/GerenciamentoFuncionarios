2f42b3511e7e0db1aa3f97c1358ae995b7e8c87f
Guilherme Castro Royer <guilherme.royer@gmail.com>
2022-01-04 20:01:32 -0300

    Criada classe Empresa para lidar com Departamentos

--- src/gestaofuncionarios/Empresa/Empresa.java
+++ src/gestaofuncionarios/Empresa/Empresa.java
@@ -1,4 +1,52 @@
 package gestaofuncionarios.Empresa;
 
-public class EmpresaTester {
+import gestaofuncionarios.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empresa {
+
+    private List<Departamento> departamentos;
+    private GerenteGeral gerenteGeral;
+
+    public Empresa(Pessoa gerente) {
+
+        departamentos = new ArrayList<>();
+        gerenteGeral = new GerenteGeral(gerente,"2022/001", "joaodasilva@gmail.com",25000);
+        Departamento dptoGerencia = new Departamento("Gerencia");
+
+        GestorFuncionarios gestorFuncionarios = new GestorFuncionarios();
+        gestorFuncionarios.adicionarFuncionario(gerenteGeral);
+        gerenteGeral.alocarDepartamento(dptoGerencia,gerenteGeral);
+    }
+
+    public boolean criaNovoDepartamento(Departamento departamento) {
+        if(departamento!= null) {
+            for(Departamento dpto : departamentos) {
+                if(dpto.getNomeDepartamento().equals(departamento.getNomeDepartamento())) {
+                    return false; // departamento ja existe
+                }
+            }
+            departamentos.add(departamento);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Departamento> listaDepartamentos() {
+        return departamentos;
+    }
+
+    public GerenteGeral getGerenteGeral() {
+        return gerenteGeral;
+    }
+
+    protected boolean setGerenteGeral(GerenteGeral gerenteGeral) {
+        if(gerenteGeral != null ) {
+            this.gerenteGeral = gerenteGeral;
+            return true;
+        }
+        return false;
+    }
 }
